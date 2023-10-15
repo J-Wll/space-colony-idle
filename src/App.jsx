7c5ea3cbd6c4d8db181faf6e57328ea381a363f5
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-15 14:18:26 +0100

    Code formatting and layout fixes

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,30 @@ export default function App() {
   var [housing, updateHousing] = useState(10000000);
   var [day, updateDay] = useState(1);
 
-function validateIncrease(variable, updateFunc, increase, maximum){
-  if (variable +increase < maximum){
-    updateFunc(variable+increase);
+  function validateIncrease(variable, updateFunc, increase, maximum) {
+    if (variable + increase < maximum) {
+      updateFunc(variable + increase);
+    }
+    else {
+      updateFunc(maximum);
+    }
   }
-  else{
-    updateFunc(maximum);
-  }
-}
-  
-  function sendColonists(a){
+
+  function sendColonists(a) {
     // Updates the specified value
     // updating as a function queues it up properly
     validateIncrease(colonists, updateColonists, shipSize, housing);
     // updateColonists(colonists => colonists + shipSize)
   }
 
-  function dailyCycle(){
+  function dailyCycle() {
     // Rounded down(0.2-1.2 * (colonists/20000) + 0.92)
     // Chances for increases even at very low pops, if random rolls 1.1+, that * 0.92 > 1
     // With 17 colonists its 0.2-1.2 * 0.92085
     // 20000 pop guarentees 1 per cycle, which i think is realistic (world pop/births per day)
-    let increase = Math.floor((Math.random()+0.11)*((colonists/21500)+0.92));
+    let increase = Math.floor((Math.random() + 0.11) * ((colonists / 21500) + 0.92));
     updateDay(day + 1);
-    console.log(((Math.random()+0.11)*((colonists/21500)+0.92)));
+    console.log(((Math.random() + 0.11) * ((colonists / 21500) + 0.92)));
     validateIncrease(colonists, updateColonists, increase, housing);
     // console.log("func", colonists)
     // if (colonists + increase < housing){
@@ -41,12 +41,12 @@ function validateIncrease(variable, updateFunc, increase, maximum){
     //   updateColonists(housing)
     // }
   }
-  
-  function upgradeShip(){
+
+  function upgradeShip() {
     updateShipSize(shipSize => shipSize + 100)
   }
 
-  function upgradeHousing(){
+  function upgradeHousing() {
     updateHousing(housing => housing + 500);
   }
 
@@ -64,40 +64,50 @@ function validateIncrease(variable, updateFunc, increase, maximum){
   return (
     <main>
       {/* <p>React ⚛️ + Vite ⚡ + Replit 🌀</p> */}
-      <p class="fs-6">Day: {day}</p>
-      <section class="stats-block">
-        <div class="stats-row">
-      <p class="stats-label border border-2">{colonists} colonists</p>
-      <p class="stats-label border border-2">Resources: </p>
-          </div>
-        <div class="stats-row">
-      <p class="stats-label border border-2">Daily growth rate: {Math.round(((colonists/21500)+0.04)*100)/100}</p>
-        <p class="stats-label border border-2">Ship size: {shipSize}</p>
+      <p className="fs-6">Day: {day}</p>
+      <section className="stats-block">
+        <div className="stats-row">
+          <p className="stats-label border border-2">{colonists} colonists</p>
+          <p className="stats-label border border-2">Resources: </p>
+        </div>
+        <div className="stats-row">
+          <p className="stats-label border border-2">Daily growth rate: {Math.round(((colonists / 21500) + 0.04) * 100) / 100}</p>
+          <p className="stats-label border border-2">Ship size: {shipSize}</p>
 
-          </div>
-        <div class="stats-row">
-      <p class="stats-label border border-2">Housing space: {housing}</p>
-      <p class="stats-label border border-2">Jobs: </p>
-          </div>
-        <div class="stats-row">
-      <p class="stats-label border border-2">Colonist efficiency: 0-1</p>
+        </div>
+        <div className="stats-row">
+          <p className="stats-label border border-2">Housing space: {housing}</p>
+          <p className="stats-label border border-2">Jobs: </p>
+        </div>
+        <div className="stats-row">
+          <p className="stats-label border border-2">Colonist efficiency: 0-1</p>
 
-          </div>
+        </div>
 
       </section>
 
-        <div class="mt-2">
-          <div class="but-label">
-        <button id="send-ship-but" onClick={() => sendColonists(1)}>Send a colonist ship (+{+shipSize})</button>
+      <div className="but-row mt-2">
+
+        <div className="but-label">
+          <button id="send-ship-but" onClick={() => sendColonists(1)}>Send a colonist ship (+{+shipSize})</button>
           <label for="send-ship-but">Cost: x resources</label>
-          </div>
-          <div class="but-label">
-        <button onClick={upgradeShip}>Expand shipyards (+100 shipsize</button>
-            </div>
         </div>
-        <div class="">
+
+        <div className="but-label">
+          <button onClick={upgradeShip}>Expand shipyards (+100 shipsize)</button>
+          <label for="">Cost: x resources</label>
+        </div>
+
+      </div>
+      <div className="but-row">
+        <div className="but-label">
         <button onClick={upgradeHousing}>Expand housing (+500)</button>
-        <button onClick={upgradeShip}>Expand shipyards (+100 shipsize)</button>
+        <label for="">Cost: x resources</label>
+        </div>
+        <div className="but-label">
+          <button onClick={upgradeShip}>Expand shipyards (+100 shipsize)</button>
+          <label for="">Cost: x resources</label>
+        </div>
       </div>
     </main>
   )
