92eb90a97abdd9377149ca5436831cbd63f3df38
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-11-07 14:00:44 +0000

    Buttons change colour based on buyability status
    Also started to converted hard coded values into flexible state values for later changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import LabelledButton from './LabelledButton';
 export default function App() {
   // [variable, functionToUpdate] = useState(defaultValue)
   let [colonists, updateColonists] = useState(17);
-  let [shipSize, updateShipSize] = useState(500);
   let [housing, updateHousing] = useState(1000);
   let [day, updateDay] = useState(1);
   let [resources, updateResources] = useState(5000);
   let [jobs, updateJobs] = useState(100)
+
+  let [shipSize, updateShipSize] = useState(500);
+  let [shipSizeCost, updateShipSizeCost] = useState(5000)
+  let [shipSizeIncrement, updateShipSizeIncrement] = useState(500)
 
   // Increases values based on if it can be afforded and if it is below the maximum
   // Parameters in order: Element being changed, function to change it, increase amount, maximium amount (default is no maximum), currency of cost (default is resources), function to update currency of cost, cost amount of the increase
@@ -26,15 +29,26 @@ export default function App() {
     }
   }
 
+  // Runs every second and is responsible for value changes
   function dailyCycle() {
-    // Rounded down(0.2-1.2 * (colonists/20000) + 0.92)
+    updateDay(day + 1);
+    validateIncrease(colonists, updateColonists, popIncreaseAmount(), housing);
+    updateResources(resourceGain());
+  }
+
+  function popIncreaseAmount(display = false) {
+    // Rounded down(0.11-1.11 * (colonists/21500) + 0.92)
     // Chances for increases even at very low pops, if random rolls 1.1+, that * 0.92 > 1
     // With 17 colonists its 0.2-1.2 * 0.92085
-    // 20000 pop guarentees 1 per cycle, which i think is realistic (world pop/births per day)
-    let increase = Math.floor((Math.random() + 0.11) * ((colonists / 21500) + 0.92));
-    updateDay(day + 1);
-    validateIncrease(colonists, updateColonists, increase, housing);
-    updateResources(resourceGain());
+    // 20000 pop guarentees 1 per cycle, which seems realistic (world pop/births per day)
+    let minimum = 0.11;
+    let popPer = 21500;
+    let baseLine = 0.92
+    // Estimated value for display, seems to be roughly accurate. Haven't worked out the correct calculation that accounts for the randomness
+    if (display === true) {
+      return Math.round(((colonists / popPer) + 0.04) * 100) / 100
+    }
+    return Math.floor((Math.random() + minimum) * ((colonists / popPer) + baseLine));
   }
 
   function resourceGain() {
@@ -48,17 +62,14 @@ export default function App() {
   }
 
   function upgradeJobs() {
-    // updateJobs(jobs => jobs + 500)
     validateIncrease(jobs, updateJobs, 500, undefined, resources, updateResources, 1000);
   }
 
   function upgradeShip() {
-    // updateShipSize(shipSize => shipSize + 100)
-    validateIncrease(shipSize, updateShipSize, 100, undefined, resources, updateResources, 1000);
+    validateIncrease(shipSize, updateShipSize, shipSizeIncrement, undefined, resources, updateResources, shipSizeCost);
   }
 
   function upgradeHousing() {
-    // updateHousing(housing => housing + 500);
     validateIncrease(housing, updateHousing, 500, undefined, resources, updateResources, 1000);
   }
 
@@ -103,7 +114,7 @@ export default function App() {
           <p className="stats-label border border-2">Jobs: {jobs}</p>
         </div>
         <div className="stats-row">
-          <p className="stats-label border border-2">Daily growth rate: {Math.round(((colonists / 21500) + 0.04) * 100) / 100}</p>
+          <p className="stats-label border border-2">Daily growth rate: {popIncreaseAmount(true)}</p>
           <p className="stats-label border border-2">Ship size: {shipSize}</p>
         </div>
         <div className="stats-row">
@@ -117,18 +128,21 @@ export default function App() {
       <div className="but-row mt-2">
         <LabelledButton onClick={() => sendColonists(1)} id="send-ship-but" className=""
           butText={`Send a colonist ship (+${colonists + shipSize > housing ? housing - colonists : shipSize})`}
-          labelText={`Cost: 1000 resources`} />
+          labelText={`Cost: 1000 resources`} resource={resources} cost={1000} />
 
         <LabelledButton onClick={upgradeShip} id="upgrade-ship-but" className=""
-          butText={`Expand shipyards (+100)`} labelText={`Cost: 1000 resources`} />
+          butText={`Expand shipyards (+${shipSizeIncrement})`} labelText={`Cost: ${shipSizeCost} resources`}
+          resource={resources} cost={shipSizeCost} />
       </div>
 
       <div className="but-row">
         <LabelledButton onClick={upgradeHousing} id="upgrade-housing-but" className=""
-          butText={`Expand housing (+500)`} labelText={`Cost: 1000 resources`} />
+          butText={`Expand housing (+500)`} labelText={`Cost: 1000 resources`}
+          resource={resources} cost={1000} />
 
-        <LabelledButton onClick={upgradeJobs}  id="upgrade-jobs-but" className=""
-        butText={`Create jobs (+500 jobs)`} labelText={`Cost: 1000 resources`}/>
+        <LabelledButton onClick={upgradeJobs} id="upgrade-jobs-but" className=""
+          butText={`Create jobs (+500 jobs)`} labelText={`Cost: 1000 resources`}
+          resource={resources} cost={1000} />
       </div>
     </main>
   )
@@ -139,3 +153,5 @@ export default function App() {
 // After purchasing it once (10000+ cost) it unlocks a new set of upgrades below the current ones
 
 // Resource cost scaling
+
+// Make daily growth rate fully accurate and not just an estimation
--- src/LabelledButton.jsx
+++ src/LabelledButton.jsx
@@ -1,8 +1,13 @@
 export default function LabelledButton(props) {
-    console.log(props.onClick)
+    let buyable;
+    if (props.resource >= props.cost){
+        buyable = "button-buyable"
+    } else {
+        buyable = "button-disabled"
+    }
     return (
         <div className="but-label">
-            <button className = {props.className} id = {props.id} onClick={props.onClick}>{props.butText}</button>
+            <button className = {`${props.className} ${buyable}`} id = {props.id} onClick={props.onClick}>{props.butText}</button>
             <label htmlFor={props.id}>{props.labelText}</label>
         </div>
     )
